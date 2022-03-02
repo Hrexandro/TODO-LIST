64094df017cdc38270259bb3577af69d71c506dd
Hrexandro <marcin_nowak_hrx@o2.pl>
2022-03-02 20:49:43 +0100

    add preliminary item display functions

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ add possible checklist input
 checklist should be checkable in the final todo interface
 add restrictions to form input to ensure aesthetic compatibility
 style everything to look nice and neat and clean and super cool
+make them draggable to switch places?
 */
 
 import './style.css';
@@ -54,7 +55,7 @@ const form = (function () {
                     appendFormElement(dueDateInput, 'Due-date', document.querySelector('label[for=Priority]'));
                 }
                 else {
-                    if (document.getElementById("Due-date")){
+                    if (document.getElementById("Due-date")) {
                         dueDateInput.remove();
                         document.querySelector('label[for=Due-date]').remove();
                     }
@@ -90,14 +91,15 @@ const form = (function () {
             document.getElementById('submit-button').addEventListener('click', () => {
                 ToDos.createToDo(titleInput.value, descriptionInput.value, prioritySelect.value, dueDateInput.value);
 
-                for (i = 0; i < document.getElementsByTagName("input").length; i++) {//clear the form values
+                for (let i = 0; i < document.getElementsByTagName("input").length; i++) {//clear the form values
                     document.getElementsByTagName("input")[i].value = "";
                     prioritySelect.value = "None"
                 }
 
                 deadLineInput.checked = false;//uncheck the deadline checkbox
                 checkCheckboxStatus(deadLineInput)//hide the date
-                DisplayingToDos.display();
+                DisplayingToDos.removeAllDisplayedContent();
+                DisplayingToDos.display(ToDos.toDoArray);
             })
         }
     })
@@ -111,15 +113,44 @@ const form = (function () {
 
 const DisplayingToDos = (function () {
 
-    function display() {
-        console.log("display todo starts")
-        for (i=0;i<ToDos.toDoArray.length;i++){
-            console.log(ToDos.toDoArray)
+    let contentDisplay = document.getElementById('content-display');
+
+    function removeAllDisplayedContent(){
+        let child = contentDisplay.lastElementChild;
+        while(child){
+            contentDisplay.removeChild(child);
+            child = contentDisplay.lastElementChild;
+        }
+    }
+
+    function display(arrayOfTodos) {
+        console.log('display todo starts');
+
+        function displayLine(label, value, parent){
+
+            let element = document.createElement('p');
+            element.innerText = `${label}: ${value}`;
+            parent.appendChild(element);
+
+        }
+        for (let j = 0; j < arrayOfTodos.length; j++) {
+            let toDoContainer = document.createElement('div');
+            contentDisplay.appendChild(toDoContainer);
+            displayLine('Title', arrayOfTodos[j].title, toDoContainer);
+            displayLine('Description', arrayOfTodos[j].description, toDoContainer);
+            if (arrayOfTodos[j].dueDate){
+                displayLine('Due date', arrayOfTodos[j].dueDate, toDoContainer);
+            }
+            if (arrayOfTodos[j].priority!=='None'){
+                displayLine("Priority", arrayOfTodos[j].priority, toDoContainer);
+            }
+            console.log(arrayOfTodos[j])
         }
     }
 
     return {
         display,
+        removeAllDisplayedContent,
     }
 })();
 
@@ -131,7 +162,7 @@ const ToDos = (function () {
             this.title = title;
             this.description = description;
             this.priority = priority;
-            if (dueDate){
+            if (dueDate) {
                 this.dueDate = dueDate;
             }
         }
